004e1256651d2dc1a84443670aec9425b417bf52
ErikBurdett <erikaburdet@gmail.com>
2021-08-24 18:09:37 -0500

    creating article models and schemas for mysql db

--- hello.py
+++ hello.py
@@ -1,13 +1,42 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flasktest'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text())
+    date = db.Column(db.DateTime, default = datetime.datetime.now)
+    def __init__(self, title,body):
+        self.title = title
+        self.body = body
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date' )
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
 
 @app.route("/get", methods = ['GET'])
 def get_articles():
     return jsonify({
         "Howdy!":"World"
     })
+
+@app.route("/add", methods = ['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
 
 @app.route("/homepage")
 def homepage():
