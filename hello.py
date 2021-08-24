dd3c188dcb972f6d64e517b945a360da471bd163
ErikBurdett <erikaburdet@gmail.com>
2021-08-24 12:14:08 -0500

    reverted to first hello world version

--- hello.py
+++ hello.py
@@ -2,22 +2,10 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-
-class HelloWorld(Resource):
-    def __init__(self):
-        pass
-    def get(self):
-        return{
-            "Hello":"World"
-        }
-
-api.add_resource(HelloWorld, '/')
-
-# @app.route("/")
-# def homepage():
-#     return "Hello Universe"
+@app.route("/")
+def homepage():
+    return "Hello Universe"
 
 if __name__== '__main__':
     app.run(debug=True)
